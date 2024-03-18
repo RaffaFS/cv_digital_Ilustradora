eb6b6ea2f9c25a6b60feceb46cf262a51031e8a1
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2024-03-18 10:35:04 -0300

    removendo a função .hover de main.js (estava criando um bug no mobile)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,4 @@ $(document).ready(function(){
         foto.toggleClass('hidden');
         codigo.toggleClass('hidden');
     });
-    $('.imgProfile').hover(function(){
-        const foto = $('.imgProfile1')
-        const codigo = $('.imgProfile2')
-        foto.toggleClass('hidden');
-        codigo.toggleClass('hidden');
-    });
 });
